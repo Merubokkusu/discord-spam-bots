691e95393596dd85cefaf4781d655aaa3522d800
Liam <Amagicalmailbox@gmail.com>
2020-01-23 02:02:42 -0500

    Update account_creator.py

--- bots/misc/account-creator/account_creator.py
+++ bots/misc/account-creator/account_creator.py
@@ -5,7 +5,7 @@
 # @license CC BY-NC-ND 3.0 US | https://creativecommons.org/licenses/by-nc-nd/3.0/us/
 # @website https://github.com/Merubokkusu/discord-spam-bots/
 # @email liam@merubokkusu.com
-# @last-modified 2020-01-14T23:09:09.764Z-05:00
+# @last-modified 2020-01-23T02:02:30.277Z-05:00
 #
 
 
@@ -20,7 +20,7 @@ from config import *
 account_Email = sys.argv[1]
 account_Password = sys.argv[2]
 PROXY = sys.argv[3]
-API_KEY = captchaAPI  # Your 2captcha API KEY
+API_KEY = captchaAPI# Your 2captcha API KEY
 site_key = '6Lef5iQTAAAAAKeIvIY-DeexoO3gj7ryl9rLMEnn' 
 url = "https://discordapp.com/api/v6/auth/register"
 
@@ -33,12 +33,11 @@ def create():
     s = requests.Session()
     captcha_id = s.post("http://2captcha.com/in.php?key={}&method=userrecaptcha&googlekey={}&pageurl={}".format(API_KEY, site_key, url)).text.split('|')[1]
     recaptcha_answer = s.get("http://2captcha.com/res.php?key={}&action=get&id={}".format(API_KEY, captcha_id)).text
-    print("solving ref captcha...")
+    print("solving captcha...")
     while 'CAPCHA_NOT_READY' in recaptcha_answer:
         sleep(5)
         recaptcha_answer = s.get("http://2captcha.com/res.php?key={}&action=get&id={}".format(API_KEY, captcha_id)).text
-    if '|' in recaptcha_answer:
-        recaptcha_answer = recaptcha_answer.split('|')[1]
+    recaptcha_answer = recaptcha_answer.split('|')[1]
    
     
 
@@ -59,9 +58,9 @@ def create():
         "captcha_key": recaptcha_answer,}
 
     r = requests.post(url,data=json.dumps(payload),headers=headers, proxies=proxy)
+    sleep(0.5)
     if(r.status_code == 201 or 200 or 202):
-        t = json.load(r.content)
         file = open('token_gen.txt','a')
-        file.writelines(t['token'] + '\n')
-    print(r.status_code)
+        file.writelines(r.json()['token'] + '\n')
+    print('Made account : '+ account_Email.split("@", 1)[0])
 create()
