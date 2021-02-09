2593c9b0622ba4dc930bb1dae2df5f13bfd748cf
Daniel S. Billing <daniel@infihex.com>
2021-02-09 15:48:37 +0100

    Update joinServer2.0.py

--- bots/misc/joinServer2.0.py
+++ bots/misc/joinServer2.0.py
@@ -15,14 +15,17 @@ import sys
 import os
 TOKEN = sys.argv[1]
 INVITE_LINK = sys.argv[2]
-PROXY = { 'http' : sys.argv[3] } 
-url = 'https://discordapp.com/api/v6/invite/'+INVITE_LINK+'?with_counts=true'
+PROXY = sys.argv[3]
+url = 'https://discord.com/api/v6/invite/'+INVITE_LINK+'?with_counts=true'
 print(url)
-headers = {"content-type": "application/json", "Authorization": TOKEN }
+headers = {"content-type": "application/json", "authorization": TOKEN }
+PROXY = {
+    'http': 'http://'+PROXY
+}
 
-r = requests.post(url,headers=headers, proxies=PROXY)
+r = requests.post(url, headers=headers, proxies=PROXY)
 if r.status_code == 200:
-    print("Token:"+"'"+TOKEN[-25]+"'"+" Joined the server")
+    print("Token: '"+TOKEN[-25]+"' Joined the server")
 else:
     print('error, something went wrong.')
     print('Make sure your token is correct | https://discordhelp.net/discord-token')
